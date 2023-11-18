392b575e93f68acd4fb779e25d838caac379f884
Bruno Zambuze <zambuzesilva@hotmail.com>
2023-11-18 10:19:19 -0300

    ExceptionExercise

---
+++ CustomExceptions/Entities/Reservation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using CustomExceptions.Entities.Exceptions;
+
+namespace CustomExceptions.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; private set; }
+        public DateTime Checkin { get; private set; }
+        public DateTime Checkout { get; private set; }
+
+        //Construtores
+        public Reservation()
+        {
+        }
+
+        public Reservation(int number, DateTime checkin, DateTime checkout)
+        {
+            DateTime now = DateTime.Now;
+            if (checkin < now || checkout < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates!");
+            }
+            if (checkin >= checkout)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+            RoomNumber = number;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        //Função duração da hospedagem
+        public int Duration()
+        {
+            TimeSpan duration = Checkin.Subtract(Checkout);
+            return (int)duration.TotalDays;
+        }
+
+        //Função para atualizara reserva
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            DateTime now = DateTime.Now;
+            if (checkin < now || checkout < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates!");
+            }
+            if (checkin >= checkout)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        //Imprimir os dados
+        public override string ToString()
+        {
+            return $"Reservation: Room {RoomNumber}, check-in: {Checkin.ToString("dd/MM/yyyy")}," +
+                $" check-out: {Checkout.ToString("dd/MM/yyyy")}, {Duration()} nights";
+        }
+    }
+}
---
+++ CustomExceptions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CustomExceptions.Entities;
+using CustomExceptions.Entities.Exceptions;
+namespace CustomExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room Number:");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/mm/yyyy):");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/mm/yyyy):");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+                Reservation reservation = new Reservation(number, checkin, checkout);
+                Console.WriteLine(reservation);
+
+                Console.WriteLine("\nEnder data to update reservation:");
+                Console.Write("Check-in date (dd/mm/yyyy):");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/mm/yyyy):");
+                checkout = DateTime.Parse(Console.ReadLine());
+                reservation.UpdateDates(checkin, checkout);
+                Console.WriteLine(reservation);
+            }
+            catch (DomainException error)
+            {
+                Console.WriteLine($"Error in reservation: {error.Message}");
+            }
+            catch (FormatException error)
+            {
+                Console.WriteLine($"Format error: {error.Message}");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"Error: {error.Message}");
+            }
+        }
+    }
+}
